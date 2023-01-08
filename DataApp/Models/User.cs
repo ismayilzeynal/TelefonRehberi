6b641679430f0d58965e28010f72b5e5163e3021
ismayilzeynal <ismayilbz@code.edu.az>
2023-01-09 01:52:10 +0400

    Main menu, Add User completed

---
+++ DataApp/Models/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataApp.Models;
+
+public partial class User
+{
+    public int EmployeeId { get; set; }
+
+    public string LastName { get; set; } = null!;
+
+    public string FirstName { get; set; } = null!;
+
+    public string? Title { get; set; }
+
+    public string? TitleOfCourtesy { get; set; }
+
+    public DateTime? BirthDate { get; set; }
+
+    public DateTime? HireDate { get; set; }
+
+    public string? EmailAddress { get; set; }
+
+    public string? City { get; set; }
+
+    public string? Phone { get; set; }
+}
---
+++ DataApp/Program.cs
@@ -0,0 +1,172 @@
+﻿using DataApp.Models;
+using System.Text.RegularExpressions;
+NorthwindContext db = new NorthwindContext();
+MainMenu();
+
+
+
+static void MainMenu()
+{
+    Console.WriteLine("-------------------- TELEFON REHBERİ --------------------\n");
+    Console.WriteLine("Kullanıcı Ekleme İşlemi   : A");
+    Console.WriteLine("Kullanıcı Listeleme       : L");
+    Console.WriteLine("Kullanıcı Arama           : S");
+    Console.WriteLine("Çıkmak İçin               : E");
+
+    string Secim = Console.ReadLine();
+
+    switch (Secim)
+    {
+        case "A":
+            ChoiseA();
+            break;
+        case "B":
+            Console.WriteLine("B seçildi");
+            break;
+        case "C":
+            Console.WriteLine("C seçildi");
+            break;
+        case "D":
+            Console.WriteLine("D seçildi");
+            break;
+        default:
+            Console.WriteLine("Yanlış girdiniz");
+            break;
+    }
+}
+
+
+
+static void ChoiseA()
+{
+    Console.Clear();
+    string Ad, Soyad, TelefonNumarasi, Email;
+    Console.Write("Kullanıcının adı: ");
+    Ad = Console.ReadLine();
+    Console.Write("Kullanıcının  Soyadı: ");
+    Soyad = Console.ReadLine();
+
+    Console.Write("Telefon: ");
+    TelefonNumarasi = Console.ReadLine();
+    if (!CheckValidPhoneNumber(TelefonNumarasi))
+        do
+        {
+            WrongInput();
+            TelefonNumarasi = Console.ReadLine();
+        } while (!CheckValidPhoneNumber(TelefonNumarasi));
+
+    Console.Write("E-mail: ");
+
+    Email = Console.ReadLine();
+    if (!CheckValidMail(Email))
+        do
+        {
+            WrongInput();
+            Email = Console.ReadLine();
+        } while (!CheckValidMail(Email));
+
+    NextA(Ad, Soyad, TelefonNumarasi, Email);
+}
+
+static void NextA(string Ad, string Soyad, string TelefonNumarasi, string Email)
+{
+    NorthwindContext db = new NorthwindContext();
+
+
+    db.Users.Add(new()
+    {
+        FirstName = Ad,
+        LastName = Soyad,
+        Phone = TelefonNumarasi,
+        EmailAddress = Email
+    });
+    db.SaveChanges();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+static bool CheckValidPhoneNumber(string str)
+{
+    string regex = @"^\+994[0-9]{9}$|^\+994[\s-][0-9]{2}[\s-][0-9]{3}[\s-][0-9]{2}[\s-][0-9]{2}$";
+    return Regex.IsMatch(str, regex, RegexOptions.IgnoreCase);
+}
+
+static bool CheckValidMail(string str)
+{
+    string regex = @"^[a-zA-Z0-9_]+@[a-zA-Z0-9]+\.[a-zA-Z]{1,3}$";
+    return Regex.IsMatch(str, regex, RegexOptions.IgnoreCase);
+}
+
+
+static void WrongInput()
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("Wrong Input! Please retry\n");
+    Console.ForegroundColor = ConsoleColor.White;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+
+
+
+
+
+
+
+
+
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
